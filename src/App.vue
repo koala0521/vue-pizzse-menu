af542e2e975635cb4ec7a93507cef57805bf9299
koala0521 <328762691@qq.com>
2018-04-07 00:00:05 +0800

    vue实现点餐系统

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+    <div class="container">
+      <app-header></app-header>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+
+  export default {
+    components:{
+    "app-header":Header
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  // template: '<App/>'
+  render:h=> h(App)
+})
+
+/*
+
+渲染组件时，template和 render都可以实现组件的渲染。 
+
+render函数的优先级，高于 template，
+
+vue2.0+中，template最终也会传入render函数中执行。
+
+*/ 
