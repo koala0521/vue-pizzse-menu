8f5a76cfab3238b8685fd089113e7b6cd9d722cc
koala0521 <328762691@qq.com>
2018-04-20 00:39:21 +0800

    购物车计算

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -18,33 +18,50 @@
           <tr v-for="option in item.options" :key="option.size">
             <td>{{option.size}}</td>
             <td>{{option.price}}</td>
-            <td><button class="btn btn-sm btn-outline-success">+</button></td>
+            <td>
+              <button 
+                class="btn btn-sm btn-outline-success"
+                @click="addItem(item , option)"
+              >+</button>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
     <!-- 购物车 -->
     <div class="col-sm-12 col-md-4">
-      <table class="table">
-        <thead class="thead-default">
-          <tr>
-            <th>数量</th>
-            <th>品种</th>
-            <th>价格</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <button class="btn btn-sm">-</button>
-              <span>1</span>
-              <button class="btn btn-sm">+</button>
-            </td>
-            <td>榴莲9寸</td>
-            <td>38</td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-show="cart.length" >
+        <table class="table">
+          <thead class="thead-default">
+            <tr>
+              <th>数量</th>
+              <th>品种</th>
+              <th>价格</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart" :key="item.name + item.size" >
+              <td>
+                <button class="btn btn-sm" @click="cartBtn(item ,'minus')" >-</button>
+                <span>{{ item.num }}</span>
+                <button class="btn btn-sm"  @click="cartBtn(item ,'add')"  >+</button>
+              </td>
+              <td>{{ item.name + item.size + "寸" }}</td>
+              <td>{{ item.price * item.num }}</td>
+            </tr>
+            <tr>
+              <td>合计：</td>
+              <td></td>
+              <td>{{ allPrice }} ￥ </td>
+            </tr>
+          </tbody>
+        </table>
+        
+      </div>
+
+      <div v-show="!cart.length" >
+        <h3>购物车空空如也！</h3>
+      </div>
     </div>
 
   </div>
@@ -55,6 +72,7 @@ export default {
   
   data(){
     return {
+      "cart":[],
       "getMenuItems":{
           1: {
             'name': '榴莲pizza',
@@ -92,8 +110,45 @@ export default {
         }
     }
   },
-  methods:{
+  "methods":{
+    addItem( item , option ){
+      console.log( item , option );
+       let obj = {
+              name:item.name,
+              num:1,
+              ...option
+            };
+      var hasItem = this.cart.find(item=>{
+          return item.name === obj.name && item.size === obj.size
+      });  
 
+      if( hasItem ){
+        
+        hasItem.num = ++hasItem.num;
+
+      }else{
+        this.cart.push( obj );
+      } 
+
+    },
+    cartBtn( item , type ){
+      item.num = type === "add" ? ++item.num  : --item.num ;
+      let index = this.cart.findIndex(el=>{
+        return el === item
+      });
+      if( item.num === 0 ){
+        this.cart.splice( index , 1 );
+      }
+    }
+  },
+  "computed":{
+    allPrice(){
+      let price = 0;
+      this.cart.forEach(item=>{
+        price += item.price * item.num;
+      });
+      return price
+    }
   }
 }
 </script>
